1e53184eab9ac7768f0fa8a6c3bd701ca2e060a3
benrau87 <benrau87@users.noreply.github.com>
2019-01-29 23:22:32 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+apt update && apt upgrade -y
+
+#Elasticsearch
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo apt-get update && sudo apt-get install elasticsearch -y
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+
+#Logstash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+apt-get update && sudo apt-get install kibana -y
+systemctl daemon-reload
+systemctl enable kibana.service
+
+#Kibana
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+apt-get update && sudo apt-get install logstash -y
+systemctl daemon-reload
+systemctl start logstash.service
+
+reboot
