b7f28a388a5aa684ce667dc45b4cb531933b3c58
benrau87 <benrau87@users.noreply.github.com>
2019-01-30 00:36:30 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,5 +33,8 @@ systemctl start logstash.service
 #Filebeat
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.6.0-amd64.deb
 sudo dpkg -i filebeat-6.6.0-amd64.deb
+systemctl daemon-reload
+systemctl start filebeat.service
+
 
 reboot
