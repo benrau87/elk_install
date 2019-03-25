965784d3d1920b89ffee1872de33e5bc394547d6
benrau87 <benrau87@users.noreply.github.com>
2019-03-25 09:23:52 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,6 +30,7 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
 debconf-set-selections
+apt update
 
 #Java and deps
 apt install -y openjdk-8-jre apt-transport-https wget oracle-java8-installer nginx apache2-utils -y 
